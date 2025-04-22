92943be5134af033ef74f408d3536c4035455849
hiig0rr <higor.batista@estudante.ifms.edu.br>
2025-04-22 18:31:23 -0400

    exercicio_01.c
    esse algoritmo identifica se o valor dos lados de um triangulo informados pelo usuario pertence a um triangulo equilátero, isósceles ou escaleno. Caso não pertença a um triangulo válido, o usuario tambem sera informado.

---
+++ exercicio_01.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main() {
+    int lado_1, lado_2, lado_3;
+    printf("Insira o lado 1");
+    scanf("%d", &lado_1);
+
+    printf("Insira o lado 2");
+    scanf("%d", &lado_2);
+
+    printf("Insira o lado 3");
+    scanf("%d", &lado_3);
+    if ((lado_1 + lado_2 > lado_3) && (lado_1 + lado_3 > lado_2) && (lado_2 + lado_3 > lado_1)) {
+        if (lado_1 == lado_2 && lado_2 == lado_3) {
+            printf("Triângulo Equilátero\n");
+        } else if (lado_1 == lado_2 || lado_1 == lado_3 || lado_2 == lado_3) {
+            printf("Triângulo Isósceles\n");
+        } else {
+            printf("Triângulo Escaleno\n");
+        }
+    } else {
+        printf("Os lados não formam um triângulo válido.\n");
+    }
+
+    return 0;
+}
